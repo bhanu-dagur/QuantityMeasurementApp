7b0691d9692c471215e1ceb67b54138e73769c0f
bhanu-dagur <bhanudagur1702@gmail.com>
2026-03-10 10:36:54 +0530

    [Bhanu Dagur]Add. Target Unit Addition

---
+++ QuantityMeasurementApp/Model/LengthUnit.cs
@@ -0,0 +1,7 @@
+public enum LengthUnit
+{
+    Feet,
+    Inch,
+    Yards,
+    Centimeters
+}
---
+++ QuantityMeasurementApp/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        QuantityMeasurementApplication.DemonstrateLengthEquality();
+        // var q1 = new QuantityLength(1.0, LengthUnit.Feet);
+        // var q2 = new QuantityLength(12.0, LengthUnit.Inch);
+
+        // var result = QuantityLength.Add(q1, q2);
+
+        // Console.WriteLine(result);
+        var q1 = new QuantityLength(1.0, LengthUnit.Feet);
+        var q2 = new QuantityLength(12.0, LengthUnit.Inch);
+
+        var result = QuantityLength.Add(q1, q2, LengthUnit.Yards);
+
+        Console.WriteLine(result);
+    }
+}
